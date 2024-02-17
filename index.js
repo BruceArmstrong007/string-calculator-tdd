0c3c9e9c552c506f0902cfa154f247dfa14a6a77
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2024-02-17 11:30:28 +0530

    first test case - add 2 numbers - fails

---
+++ index.js
@@ -0,0 +1,6 @@
+function add(operands) {
+
+}
+
+
+module.exports = add;
--- tests/string-calculator.test.js
+++ tests/string-calculator.test.js
@@ -1,5 +1,12 @@
 describe('testing string calculator',() => {
 
+    beforeEach(() => {
+        const sum = require('./../index');
+    });
+
+    it('adds 2 numbers', () => {
+        expect(sum(1, 2)).toBe(3)
+    })
 
 
 })
