6fb61daeb0881308433e662065785c266019a863
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2024-02-17 11:48:21 +0530

    second test case - add 2 numbers even if string empty - passed

--- tests/string-calculator.test.js
+++ tests/string-calculator.test.js
@@ -5,8 +5,11 @@ describe('testing string calculator',() => {
 
 
     it('adds 2 numbers', () => {
-        expect(sum('1,2')).toBe(3)
+        expect(sum('1,2')).toBe(3);
     });
 
+    it('add numbers even if the number string is empty', () => {
+        expect(sum(',2,3')).toBe(5);
+    })
 
-})
+});
