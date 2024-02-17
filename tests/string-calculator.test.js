13d5c410e5226d33042c33d6a1ea72767c3656d2
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2024-02-17 11:57:04 +0530

    3.2 test case - add method should work when  new line characters included without number strings - failed

--- tests/string-calculator.test.js
+++ tests/string-calculator.test.js
@@ -12,8 +12,12 @@ describe('testing string calculator',() => {
         expect(sum(',2,3')).toBe(5);
     })
 
-    it('add method should handle new line characters (\n)', () => {
+    it('add method should handle new line characters (\n) passed with number strings', () => {
         expect(sum('1,2\n,3')).toBe(6);
+    })
+
+    it('add method should handle new line characters (\n) passed with empty strings', () => {
+        expect(sum('\n,2\n,3')).toBe(6);
     })
 
 });
